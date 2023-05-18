8216e81147cf397337a5aed2d93554ca0b159eb9
sanjanas18 <94811483+sanjanas18@users.noreply.github.com>
2023-05-18 09:45:24 -0700

    fox riddle

--- FinalProject/fox.gd
+++ FinalProject/fox.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 func _ready():
 	$AnimatedSprite2D.play("idle")
+	var nodetwo = get_node("../Player/boxtext")
+	nodetwo.text = "Beware of Enemies Beyond, save the creature if you may. Escape you must, find the gem to continue your day."
 
 
 
@@ -13,7 +15,14 @@ func _on_area_2d_body_entered(body):
 		print("true")
 		var nodeone = get_node("../Player/box")
 		var nodetwo = get_node("../Player/boxtext")
+		var animation = get_node("../Player/AnimationPlayer")
 		print(nodeone)
 		nodeone.show()
 		nodetwo.show()
+		animation.play("typing")
+		await get_tree().create_timer(3.0).timeout
+		nodetwo.hide()
+		nodeone.hide()
+		
+		
 		
