b9898eedfa0f548ca585ce22226ff0c77204d6c1
sanjanas18 <94811483+sanjanas18@users.noreply.github.com>
2023-05-20 15:55:38 -0400

    level 2 controlling script
    did level two winning code

---
+++ FinalProject/Levels/LevelTwo.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var winscreen = get_node("Player/WinScreen")
+	#print(winscreen)
+	
+
+
+func _on_win_trigger_body_entered(body):
+	print(Globals.characteraray)
+	if body is Player:
+		print("the player reached")
+		var winscreen = get_node("Player/WinScreen")
+		var wintext = get_node("Player/WinText")
+		var winbutton = get_node("Player/WinButton")
+		winscreen.show()
+		wintext.show()
+		winbutton.show()
+		Globals.levels_done = 1 
+		Globals.save()
+		
+
+
+
+func _on_win_button_pressed():
+	get_tree().change_scene_to_file("res://MainMenu.tscn")
