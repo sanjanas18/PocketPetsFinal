4dc46a554e0c9d9d89d8871e11ea3f563fa1e304
sanjanas18 <94811483+sanjanas18@users.noreply.github.com>
2023-05-22 21:48:10 -0700

    level selector continued
    shows sprites based on level saved

--- FinalProject/LevelSelector.gd
+++ FinalProject/LevelSelector.gd
@@ -9,8 +9,19 @@ func _ready():
 	var index = 1
 	while index <= levels:
 		print(index)
+		#show the level
+		var node = get_node("level_"+str(index))
+		node.show()
+		#print(levels)
+		if index == levels:
+			print(index+1)
+			var nextindex = index + 1
+			
+			var next = get_node("level_" + str(nextindex))
+			next.show()
 		#add show the level
 		index+=1
+		
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
