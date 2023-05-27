5f3a553e538db543ccc38e481c74a7622d7cfc7a
sanjanas18 <94811483+sanjanas18@users.noreply.github.com>
2023-05-27 12:43:45 -0700

    level two start, tutorial almost done

---
+++ FinalProject/Tutorial.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_done_trigger_body_entered(body):
+	if body is Player:
+		
+		var WinScreen = get_node("Player/WinScreen")
+		var WinText = get_node("Player/WinText")
+		var WinButton = get_node("Player/WinButton")
+		WinScreen.show()
+		WinText.show()
+		WinButton.show()
